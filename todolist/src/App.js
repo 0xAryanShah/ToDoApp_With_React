011e31b04a42d3544b135805be1b2c998b59c706
Aryan Shah <05aryanshah@gmail.com>
2022-05-01 03:02:37 +0530

    Day1 till Creating New Task

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,9 +1,22 @@
-
+import TaskList from "./TaskList";
 function App() {
+  const tasks = [
+    {id:0,description:'do this',done:false},
+    {id:1,description:'do that',done:false},
+  ];
   return (
-    <div>
-      <h1>Aryan First App</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="container-fluid bg-dark text-light p-5">
+            <h1 className="display-4">TO DO List</h1>
+            <hr/>
+          </div>
+          <TaskList tasks={tasks} />
+        </div>
+      </div>           
     </div>
+    
   );
 }
 
---
+++ todolist/src/TaskList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Task from "./Task";
+export default({ tasks }) => {
+    return(
+        <ul className="list-group">
+            {tasks.map(task => (
+                <li key={task.id} className="list-group-item">
+                    <Task task={task} />                    
+                </li>
+            ))};                        
+        </ul>
+    );
+}
